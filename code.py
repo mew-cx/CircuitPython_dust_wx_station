05d5aad7ecd616bd72d9e9a0664eff552d39c178
Mike Weiblen <mew@mew.cx>
2022-06-12 18:04:03 -0600

    code.py

--- code.py
+++ code.py
@@ -1,8 +1,11 @@
 #import ds1307_simpletest
 #import htu21d_simpletest
 import mpl3115a2_simpletest
+#rfc5424_formatter.py
 #import sps30_simpletest
 
 #import i2c_power_disable
 #import i2c_power_test
 #import sps30_test
+
+# eof
