11bad57b0653c2c4c8856a22824f3b2f7405af35
Mike Weiblen <mew@mew.cx>
2022-06-29 02:01:27 -0600

    split out TheApp

--- main.py
+++ main.py
@@ -42,105 +42,134 @@ from secrets import secrets
 
 #############################################################################
 
-# SPS30 limits I2C rate to 100kHz
-i2c = busio.I2C(board.SCL, board.SDA, frequency=100_000)
-# I2C addresses found: 0xb, 0x40, 0x60, 0x68, 0x69
+class TheApp:
+    "The top-level application code for the 'dust' weather station"
 
-# Create the I2C sensor instances
-ds1307  = adafruit_ds1307.DS1307(i2c)        # id 0x68
-htu21d  = adafruit_htu21d.HTU21D(i2c)        # id 0x40
-mpl3115 = adafruit_mpl3115a2.MPL3115A2(i2c)  # id 0x60
-sps30   = SPS30_I2C(i2c, fp_mode=True)       # id 0x69
+    def __init__(self):
+        self.NUM_DOTS = 4
+        self.dots    = None
+        self.ds1307  = None
+        self.htu21d  = None
+        self.mpl3115 = None
+        self.sps30   = None
+        self.HOST    = "pink"
+        self.PORT    = 514
+        self.ws      = None
 
-# dotstar strip on hardware SPI
-NUM_DOTS = 4
-dots = adafruit_dotstar.DotStar(board.SCK, board.MOSI, NUM_DOTS, brightness=0.1)
+    def SetDots(self, r, g, b):
+        for dot in range(self.NUM_DOTS):
+            self.dots[dot] = (r,g,b)
 
-#############################################################################
+    def InitializeDevices(self):
+        # hardware SPI controls the 4-LED dotstar strip
+        self.dots = adafruit_dotstar.DotStar(board.SCK, board.MOSI, self.NUM_DOTS, brightness=0.1)
+        self.SetDots(0,255,0)
 
-def InitializeDevices():
-    # Turn off I2C VSENSOR to save power
-    i2c_power = digitalio.DigitalInOut(board.I2C_POWER)
-    i2c_power.switch_to_input()
+        # Turn off onboard D13 red LED to save power
+        led = digitalio.DigitalInOut(board.LED)
+        led.direction = digitalio.Direction.OUTPUT
+        led.value = False
 
-    # Turn off onboard D13 red LED to save power
-    led = digitalio.DigitalInOut(board.LED)
-    led.direction = digitalio.Direction.OUTPUT
-    led.value = False
+        # Turn off I2C VSENSOR to save power
+        i2c_power = digitalio.DigitalInOut(board.I2C_POWER)
+        i2c_power.switch_to_input()
 
-    # Turn off onboard NeoPixel to save power
-    pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
-    pixel.brightness = 0.0
-    pixel.fill((0, 0, 0))
-    # TODO disable board.NEOPIXEL_POWER
+        # Turn off onboard NeoPixel to save power
+        pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+        pixel.brightness = 0.0
+        pixel.fill((0, 0, 0))
+        # TODO disable board.NEOPIXEL_POWER
 
-    # Don't care about altitude, so use Standard Atmosphere [pascals]
-    mpl3115.sealevel_pressure = 101325
+        # The SPS30 limits the I2C bus rate to 100kHz
+        i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+
+        # Create the I2C sensor instances
+        self.ds1307  = adafruit_ds1307.DS1307(i2c)        # id 0x68
+        self.htu21d  = adafruit_htu21d.HTU21D(i2c)        # id 0x40
+        self.mpl3115 = adafruit_mpl3115a2.MPL3115A2(i2c)  # id 0x60
+        self.sps30   = SPS30_I2C(i2c, fp_mode=True)       # id 0x69
+
+        # Don't care about altitude, so use Standard Atmosphere [pascals]
+        # mpl3115.sealevel_pressure = 101325
+
+        self.SetDots(255,0,0)
+
+        # Create a socket to the syslog server.
+        self.ws = wifi_socket.WifiSocket(self.HOST, self.PORT)
+        self.ws.ConnectToAP(secrets["ssid"], secrets["password"])
+        self.ws.ConnectToSocket()
+
+    def Shutdown(self):
+        self.SetDots(0,0,0)
+        # what other shutdown tasks?
+
+    def WriteToSyslog(self, message, severity=rfc5424.Severity.INFO):
+        syslog_msg = rfc5424.FormatSyslog(
+            facility = rfc5424.Facility.LOCAL3,
+            severity = severity,
+            timestamp = rfc5424.FormatTimestamp(self.ds1307.datetime),
+            hostname = self.ws.ipaddr,
+            app_name = "dust",
+            msg = message)
+        # TODO handle ECONNECT exception
+        self.ws.socket.send(syslog_msg + b'\n')
+
+    def WriteCsvHeaders(self):
+        "Write column headers for CSV data via syslog"
+        self.WriteToSyslog('"timestamp","temp[C]","RH[%]","pres[pa]","tps[um]",' \
+                      '"1.0um mass[ug/m^3]","2.5um mass[ug/m^3]","4.0um mass[ug/m^3]",' \
+                      '"10um mass[ug/m^3]",' \
+                      '"0.5um count[#/cm^3]","1.0um count[#/cm^3]","2.5um count[#/cm^3]",' \
+                      '"4.0um count[#/cm^3]","10um count[#/cm^3]"')
+
+    def WriteCsvData(self, csv_msg):
+        "Write sensor data in CSV format via syslog"
+        self.WriteToSyslog(csv_msg)
+        self.SetDots(0,0,0)
+
+    def AcquireData(self):
+        self.SetDots(0,0,255)
+
+        ts = rfc5424.FormatTimestamp(self.ds1307.datetime)
+
+        h = "{:0.1f},{:0.1f},{:0.0f},".format(
+            self.htu21d.temperature,
+            self.htu21d.relative_humidity,
+            self.mpl3115.pressure)
+
+        x = self.sps30.read()
+#        try:
+#            x = self.sps30.read()
+#            #print(x)
+#        except RuntimeError as ex:
+#            print("Cant read SPS30, skipping: " + str(ex))
+#            continue
+
+        p1 = "{:0.3f},".format(x["tps"])
+        p2 = "{:0.1f},{:0.1f},{:0.1f},{:0.1f},".format(
+            x["pm10 standard"], x["pm25 standard"], x["pm40 standard"], x["pm100 standard"])
+        p3 = "{:0.0f},{:0.0f},{:0.0f},{:0.0f},{:0.0f}".format(
+            x["particles 05um"], x["particles 10um"], x["particles 25um"],
+            x["particles 40um"], x["particles 100um"])
+
+        result = '"' + ts + '",' + h + p1 + p2 + p3
+        return result
 
 #############################################################################
 
 @atexit.register
 def shutdown():
-    for dot in range(NUM_DOTS):
-        dots[dot] = (0,0,0)
+    app.Shutdown()
 
 #############################################################################
 # main
 
-HOST = "pink"
-PORT = 514
-TIMEOUT = 5
-
-ws = wifi_socket.WifiSocket(HOST, PORT)
-ws.ConnectToAP(secrets["ssid"], secrets["password"])
-ws.ConnectToSocket()
-
-InitializeDevices()
-
-# Write column headers for CSV data via syslog
-ws.socket.send(rfc5424.FormatSyslog(
-    facility = rfc5424.Facility.LOCAL3,
-    severity = rfc5424.Severity.INFO,
-    timestamp = rfc5424.FormatTimestamp(ds1307.datetime),
-    hostname = ws.ipaddr,
-    app_name = "dust",
-    msg = '"timestamp","temp[C]","RH[%]","pres[pa]","tps[um]",' \
-          '"1.0um mass[ug/m^3]","2.5um mass[ug/m^3]","4.0um mass[ug/m^3]","10um mass[ug/m^3]",' \
-          '"0.5um count[#/cm^3]","1.0um count[#/cm^3]","2.5um count[#/cm^3]","4.0um count[#/cm^3]","10um count[#/cm^3]"'
-    ) + b'\n')
-
+app = TheApp()
+app.InitializeDevices()
+app.WriteCsvHeaders()
 while True:
-    ts = rfc5424.FormatTimestamp(ds1307.datetime)
-
-    h = "{:0.1f},{:0.1f},{:0.0f},".format(
-        htu21d.temperature, htu21d.relative_humidity, mpl3115.pressure)
-
-    try:
-        x = sps30.read()
-        #print(x)
-    except RuntimeError as ex:
-        print("Cant read SPS30, skipping: " + str(ex))
-        continue
-
-    p1 = "{:0.3f},".format(x["tps"])
-    p2 = "{:0.1f},{:0.1f},{:0.1f},{:0.1f},".format(
-        x["pm10 standard"], x["pm25 standard"], x["pm40 standard"], x["pm100 standard"])
-    p3 = "{:0.0f},{:0.0f},{:0.0f},{:0.0f},{:0.0f}".format(
-        x["particles 05um"], x["particles 10um"], x["particles 25um"],
-        x["particles 40um"], x["particles 100um"])
-
-    result = '"' + ts + '",' + h + p1 + p2 + p3
-
-    # Write sensor data in CSV format via syslog
-    sent = ws.socket.send(rfc5424.FormatSyslog(
-        facility = rfc5424.Facility.LOCAL3,
-        severity = rfc5424.Severity.INFO,
-        timestamp = ts,
-        hostname = ws.ipaddr,
-        app_name = "dust",
-        msg = result
-        ) + b'\n')
-
+    result = app.AcquireData()
+    app.WriteCsvData(result)
     time.sleep(5*60)
 
 # vim: set sw=4 ts=8 et ic ai:
